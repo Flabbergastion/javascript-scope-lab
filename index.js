f553036e91de48ac01269f486ecc0a9e907b05ea
Flabbergastion <maxgralpois@gmail.com>
2025-08-12 17:25:26 -0700

    Fixed Syntax Errors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your solution in this file!
 const burgers = ["Hamburger", "Cheeseburger"]
-const featuredDrink = "Strawberry Milkshake"
+let featuredDrink = "Strawberry Milkshake"
 
 function addBurger() {
-    let newBurger = "Flatburger"
+    const newBurger = "Flatburger"
     burgers.push(newBurger);
 }
 if (true) {
-  let anothernewBurger = 'Maple Bacon Burger';
+  const anothernewBurger = 'Maple Bacon Burger';
   burgers.push(anothernewBurger);
 
   function changeFeaturedDrink() {
