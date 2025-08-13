64d8d3f4091e71a9c8c992c3a7bb5ac8409e77a5
Flabbergastion <maxgralpois@gmail.com>
2025-08-12 17:20:53 -0700

    Created all necessary code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"]
+const featuredDrink = "Strawberry Milkshake"
+
+function addBurger() {
+    let newBurger = "Flatburger"
+    burgers.push(newBurger);
+}
+if (true) {
+  let anothernewBurger = 'Maple Bacon Burger';
+  burgers.push(anothernewBurger);
+
+  function changeFeaturedDrink() {
+    FeaturedDrink = 'The JavaShake';
+  }
+}
